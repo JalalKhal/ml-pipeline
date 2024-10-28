70e24c20be8f650b2886fa65b8d521df52977b51
JalalKhal <101316722+JalalKhal@users.noreply.github.com>
2024-10-28 11:27:44 +0100

    Update split.py

--- ml_pipeline/tvs/fail_psf/steps/split.py
+++ ml_pipeline/tvs/fail_psf/steps/split.py
@@ -1,9 +1,5 @@
 """
 This module defines the following routines used by the 'split' step of the regression recipe:
-
-- ``create_dataset_filter``: Defines customizable logic for filtering the training
-  datasets produced by the data splitting procedure. Note that arbitrary transformations
-  should go into the transform step.
 """
 
 from ml_easy.recipes.classification.v1.config import ClassificationSplitConfig
